ca62f5de314e46a5f13653e58d46796a2986fa01
Christopher Poole <mail@christopherpoole.net>
2013-03-13 14:12:47 +1000

    added comments to gamma evaluation algoithm, calculation of slices probably needs more explaination

--- dta/algorithms.py
+++ dta/algorithms.py
@@ -62,23 +62,48 @@ def gamma_evaluation(sample, reference, distance, threshold, resolution, signed=
         "`sample` and `reference` dimensions must equal `resolution` length"
     assert sample.shape == reference.shape, \
         "`sample` and `reference` must have the same shape"
-    
+
+    # First we need to construct the distance penalty kernel, for this we use
+    # a meshgrid. The trick is creating the appropriate slices.
+
+    # We require one slice per dimension and around the current point
+    # between -distance/resolution and +distance/resolution. We transpose (.T)
+    # so we have a colum vector.
     resolution = numpy.array(resolution)[[numpy.newaxis for i in range(ndim)]].T
     slices = [slice(-ceil(distance/r), ceil(distance/r)+1) for r in resolution]
     
+    # Scale the meshgide to the resolution of the images, google "numpy.mgrid"
     kernel = numpy.mgrid[slices] * resolution
-    kernel = numpy.sum(kernel**2, axis=0) # Distance squared from central voxel.
+
+    # Distance squared from the central voxel
+    kernel = numpy.sum(kernel**2, axis=0)
+
+    # If the distance from the central voxel is greater than the distance
+    # threshold, set to infinity so that it will not be selected as the minimum
     kernel[numpy.where(numpy.sqrt(kernel) > distance)] = numpy.inf
+
+    # Divide by the square of the distance threshold, this is the cost penalty
     kernel = kernel / distance**2
-    
+  
+    # ndimag.generic_filter needs to know the footprint of the
+    # kernel which must be flat
     footprint = numpy.ones_like(kernel)
     kernel = kernel.flatten()
+
+    # Apply the dose threshold penalty (see gamma evaluation equation), here
+    # we are still under the sqrt.
     values = (reference - sample)**2 / (threshold)**2
     
+    # Move the distance penalty kernel over the dose penalised values and search
+    # for the minimum of the sum between the kernel and the values under it. This
+    # is the point of closest agreement.
     gamma_map = generic_filter(values, \
         lambda vals: numpy.minimum.reduce(vals + kernel), footprint=footprint)
+
+    # Euclidean distance
     gamma_map = numpy.sqrt(gamma_map)
 
+    # Rough signed gamma evaluation.
     if (signed):
         return gamma_map * numpy.sign(sample - reference)
     else:
